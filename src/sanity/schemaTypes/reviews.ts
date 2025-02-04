4201421e0ad33442bda5c0df2fa48808a161db49
Maheen-Zubair <sanazubair071@gmail.com>
2025-02-05 03:18:30 +0500

    admin-panel

--- src/sanity/schemaTypes/reviews.ts
+++ src/sanity/schemaTypes/reviews.ts
@@ -1,3 +1,5 @@
+import { Rule } from "sanity";
+
 export default {
     name: "review",
     title: "Reviews",
@@ -5,7 +7,7 @@ export default {
     fields: [
       { name: "name", title: "Reviewer Name", type: "string" },
       { name: "comment", title: "Comment", type: "text" },
-      { name: "rating", title: "Rating", type: "number", validation: (Rule:any) => Rule.min(1).max(5) },
+      { name: "rating", title: "Rating", type: "number", validation: (Rule:Rule) => Rule.min(1).max(5) },
       {
         name: "product",
         title: "Product",
